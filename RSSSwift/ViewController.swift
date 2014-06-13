5373dbaefaf630636daf4b559296bb3cd779705c
muukii <muukii.muukii@gmail.com>
2014-06-13 13:36:30 +0900

    Initial Commit

---
+++ RSSSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSSSwift
+//
+//  Created by Muukii on 6/13/14.
+//  Copyright (c) 2014 Muukii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
